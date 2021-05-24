f484572adcf74667350e70225ab3e1880dc09258
Alexandre <autran84312@gmail.com>
2021-05-24 17:03:04 +0200

    [InfoSection] updated

--- src/components/InfoSection/InfoElements.js
+++ src/components/InfoSection/InfoElements.js
@@ -2,10 +2,11 @@ import styled from 'styled-components'
 
 export const InfoContainer = styled.div`
   color: #fff;
-  background: ${({LightBg}) => (LightBg ? '#f9f9f9' : '#010606')};
+  background: ${({lightBg}) => (lightBg ? '#f9f9f9' : '#010606')};
+  
   @media screen and (max-width: 768px) {
-    padding: 100px 0;
-  }
+    padding: 100px 0px;
+  };
 `;
 
 export const InfoWrapper = styled.div`
@@ -16,7 +17,7 @@ export const InfoWrapper = styled.div`
   max-width: 1100px;
   margin-right: auto;
   margin-left: auto;
-  padding: 0 24px;
+  padding: 0px 24px;
   justify-content: center;
 `;
 
@@ -24,7 +25,7 @@ export const InfoRow = styled.div`
   display: grid;
   grid-auto-columns: minmax(auto, 1fr);
   align-items: center;
-  grid-template-areas: ${({imgStart}) => (imgStart ? `'col2 col1'` : `'col1 col2`)};
+  grid-template-areas: ${({imgStart}) => (imgStart ? `'col2 col1'` : `'col1 col2'`)};
   
   @media screen and (max-width: 768px) {
     grid-template-areas: ${({imgStart}) => (imgStart ? `'col1' 'col2'` : `'col1 col1' 'col2 col2'`)};
@@ -33,19 +34,19 @@ export const InfoRow = styled.div`
 
   export const Column1 = styled.div`
     margin-bottom: 15px;
-    padding: 0 15px;
+    padding: 0px 15px;
     grid-area: col1;
   `;
 
   export const Column2 = styled.div`
     margin-bottom: 15px;
-    padding: 0 15px;
+    padding: 0px 15px;
     grid-area: col2;
   `;
 
   export const TextWrapper = styled.div`
     max-width: 540px;
-    padding-top: 0;
+    padding-top: 0px;
     padding-bottom: 60px;
   `;
 
@@ -90,6 +91,6 @@ export const InfoRow = styled.div`
 
   export const Img = styled.img`
     width: 100%;
-    margin: 0 0 10px 0;
-    padding-right: 0;
+    margin: 0px 0px 10px 0px;
+    padding-right: 0px;
   `;
