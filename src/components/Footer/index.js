36630f1701c5844622e4a763005f106b95ecfe11
Alexandre <autran84312@gmail.com>
2021-05-26 21:20:04 +0200

    [Footer] Initialized

---
+++ src/components/Footer/index.js
@@ -0,0 +1,77 @@
+import React from 'react'
+import { FaFacebook, FaInstagram, FaLinkedin, FaTwitter, FaYoutube } from 'react-icons/fa'
+import { FooterContainer, FooterWrap, FooterLinksContainer, FooterLinksWrapper, FooterLinkItems, FooterLinkTitle, FooterLink, SocialMedia, SocialMediaWrap, SocialLogo, WebsiteRights, SocialIcons, SocialIconLink} from './FooterElements'
+
+const Footer = () => {
+  return (
+    <FooterContainer>
+      <FooterWrap>
+        <FooterLinksContainer>
+          <FooterLinksWrapper>
+            <FooterLinkItems>
+              <FooterLinkTitle>About Us</FooterLinkTitle>
+                <FooterLink to='/signin'>How it works</FooterLink>
+                <FooterLink to='/'>Testimonials</FooterLink>
+                <FooterLink to='/'>Carrers</FooterLink>
+                <FooterLink to='/'>Investors</FooterLink>
+                <FooterLink to='/'>Terms of Services</FooterLink>
+            </FooterLinkItems>
+            <FooterLinkItems>
+              <FooterLinkTitle>Contact Us</FooterLinkTitle>
+                <FooterLink to='/'>How it works</FooterLink>
+                <FooterLink to='/'>Testimonials</FooterLink>
+                <FooterLink to='/'>Carrers</FooterLink>
+                <FooterLink to='/'>Investors</FooterLink>
+                <FooterLink to='/'>Terms of Services</FooterLink>
+            </FooterLinkItems>
+          </FooterLinksWrapper>
+          <FooterLinksWrapper>
+            <FooterLinkItems>
+              <FooterLinkTitle>About Us</FooterLinkTitle>
+                <FooterLink to='/'>How it works</FooterLink>
+                <FooterLink to='/'>Testimonials</FooterLink>
+                <FooterLink to='/'>Carrers</FooterLink>
+                <FooterLink to='/'>Investors</FooterLink>
+                <FooterLink to='/'>Terms of Services</FooterLink>
+            </FooterLinkItems>
+            <FooterLinkItems>
+              <FooterLinkTitle>About Us</FooterLinkTitle>
+                <FooterLink to='/'>How it works</FooterLink>
+                <FooterLink to='/'>Testimonials</FooterLink>
+                <FooterLink to='/'>Carrers</FooterLink>
+                <FooterLink to='/'>Investors</FooterLink>
+                <FooterLink to='/'>Terms of Services</FooterLink>
+            </FooterLinkItems>
+          </FooterLinksWrapper>
+        </FooterLinksContainer>
+        <SocialMedia>
+          <SocialMediaWrap>
+            <SocialLogo to='/'>
+              Ecocoricoo
+            </SocialLogo>
+            <WebsiteRights>Ecocoricoo © {new Date().getFullYear()} All rights reserved.</WebsiteRights>
+            <SocialIcons>
+              <SocialIconLink href="/" target="_blank" aria-label="Facebook">
+                <FaFacebook />
+              </SocialIconLink>
+              <SocialIconLink href="/" target="_blank" aria-label="Instagram">
+                <FaInstagram />
+              </SocialIconLink>
+              <SocialIconLink href="/" target="_blank" aria-label="Youtube">
+                <FaYoutube />
+              </SocialIconLink>
+              <SocialIconLink href="//www.twitter.com" target="_blank" aria-label="Twitter">
+                <FaTwitter />
+              </SocialIconLink>
+              <SocialIconLink href="/" target="_blank" aria-label="Linkedin">
+                <FaLinkedin />
+              </SocialIconLink>
+            </SocialIcons>
+          </SocialMediaWrap>
+        </SocialMedia>
+      </FooterWrap>
+    </FooterContainer>
+  )
+}
+
+export default Footer
