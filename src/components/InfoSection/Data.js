2c40b2af2d7257cf2452043cbf1cb56b01550754
Alexandre <autran84312@gmail.com>
2021-05-26 11:36:43 +0200

    [InfoElement] updated

--- src/components/InfoSection/Data.js
+++ src/components/InfoSection/Data.js
@@ -16,3 +16,37 @@ export const homeObjOne = {
   primary: true,
   darkText: false
 };
+
+export const homeObjTwo = {
+  id: 'discover',
+  lightBg: true,
+  lightText: false,
+  lightTextDesc: false,
+  topLine: 'Consomm\'Acteur',
+  headline: '2Nous rejoindre comme consomm\'Acteurs?',
+  description: 'Nous croyons en un monde nouveau, où les consommateurs deviendraient des acteurs pour la planète. Nous voulons acheter des produits de saisons, tout en connaissant leurs producteurs. Nous sommes prêts à donner de notre temps 2-3 fois par an.',
+  buttonLabel: 'En apprendre +',
+  imgStart: true,
+  img: image1,
+  alt: 'Agriculteur',
+  dark: false,
+  primary: false,
+  darkText: true
+};
+
+export const homeObjThree = {
+  id: 'signup',
+  lightBg: false,
+  lightText: true,
+  lightTextDesc: true,
+  topLine: 'Consomm\'Acteur',
+  headline: '3Nous rejoindre comme consomm\'Acteurs?',
+  description: 'Nous croyons en un monde nouveau, où les consommateurs deviendraient des acteurs pour la planète. Nous voulons acheter des produits de saisons, tout en connaissant leurs producteurs. Nous sommes prêts à donner de notre temps 2-3 fois par an.',
+  buttonLabel: 'Commencer',
+  imgStart: false,
+  img: image1,
+  alt: 'Consomm\'Acteur',
+  dark: true,
+  primary: true,
+  darkText: false
+};
